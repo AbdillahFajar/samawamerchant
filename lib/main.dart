bb3c609f4b6b7dadf5b651bb102d9c42bd23ba84
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-04-24 18:38:53 +0700

    feat: initialize themeProvider variabel
    add themeProvider variabel to rebuild application theme

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,12 @@ import 'package:samawamerchant/features/cart/data/repositories/cart_repository_i
 import 'package:samawamerchant/features/orders/data/repositories/order_repository_impl.dart';
 import 'firebase_options.dart';
 import 'features/auth/presentation/providers/auth_provider.dart';
-import 'core/routes/app_router.dart';
 import 'features/catalog/presentation/providers/product_provider.dart';
 import 'features/cart/presentation/providers/cart_provider.dart';
 import 'features/orders/presentation/providers/order_provider.dart';
-import '../../core/theme/app_theme.dart';
+import 'core/routes/app_router.dart';
+import 'core/theme/app_theme.dart';
+import 'core/providers/theme_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +20,13 @@ void main() async {
 
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => AuthProvider())],
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+        ChangeNotifierProvider(create: (_) => CartProvider(repository: CartRepositoryImpl())),
+        ChangeNotifierProvider(create: (_) => OrderProvider(OrderRepositoryImpl())),
+      ],
       child: const MyApp(), //bikin MyApp file
     ),
   );
@@ -30,20 +37,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (_) => ProductProvider()),
-        ChangeNotifierProvider(create: (_) => CartProvider(repository: CartRepositoryImpl())),
-        ChangeNotifierProvider(create: (_) => OrderProvider(OrderRepositoryImpl())),
-      ],
-      child: MaterialApp(
+    //Buat variabel themeProvider untuk rebuild aplikasi menggunakan consumer watch dari package provider
+    final themeProvider = context.watch<ThemeProvider>();
+
+    return MaterialApp(
         title: 'Samawa Merchant',
-        debugShowCheckedModeBanner: false,
+        
+        //Daftarkan kedua tema aplikasi
         theme: AppTheme.light,
+        darkTheme: AppTheme.dark,
+
+        //Untuk menentukan mode tema yang digunakan dengan menggunakan themeProvider. Perubahan modenya dipicu oleh fungsi toggleTheme() dari class ThemeProvider yang 
+        themeMode: themeProvider.themeMode,
+
         initialRoute: AppRouter.splash,
         routes: AppRouter.routes,
-      ),
+        debugShowCheckedModeBanner: false,
     );
   }
 }
