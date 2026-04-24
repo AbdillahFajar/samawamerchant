edd996caa8b6a58e8922c3042a8991a5cf38bade
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-04-24 18:23:53 +0700

    docs: app colors
    add comments

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -12,31 +12,34 @@ class AppColors {
   //End of Main Colors
 
   //Light Mode
-  static const Color lightBackground = Color(0xFFF5F5F5);
-  static const Color lightSurface = Colors.white;
+  static const Color lightBackground = Color(0xFFF5F5F5); //Putih ke abu-abu
+  static const Color lightSurface = Colors.white; //Putih
 
   //Text Colors
-  static const Color textPrimary = Color(0xFF212121);
-  static const Color textSecondary = Color(0xFF757575);
-  static const Color textHint = Color(0xFFBDBDBD);
+  static const Color lightTextPrimary = Color(0xFF212121); //Hitam atau abu-abu sangat gelap
+  static const Color lightTextSecondary = Color(0xFF757575); //Abu-abu
+  static const Color lightTextHint = Color(0xFFBDBDBD); //Abu-abu terang
 
   //Line (Divider and Border) Colors
-  static const Color divider = Color(0xFFE0E0E0);
-  static const Color border = Color(0xFFE0E0E0);
+  static const Color lightDivider = Color(0xFFE0E0E0); //Putih atau abu-abu muda
+  static const Color lightBorder = Color(0xFFE0E0E0); //Putih atau abu-abu muda
   //End of Light Mode
 
   //Dark Mode
-  static const Color darkBackground = Color(0xFF121212);
-  static const Color darkSurface = Color(0xFF1E1E1E);
-  static const Color darkSurfaceCard = Color(0xFF2C2C2C);
+  static const Color darkBackground = Color(0xFF121212); //Abu sangat gelap
+  static const Color darkSurface = Color(0xFF1E1E1E); //Abu-abu gelap
+  static const Color darkSurfaceCard = Color(0xFF2C2C2C); //Abu-abu gelap
   
   //Text Colors
-  static const Color darkTextPrimary = Color(0xFFEEEEEE);
-  static const Color darkTextSecondary = Color(0xFFAAAAAA);
-  static const Color darkTextHint = Color(0xFF666666);
+  static const Color darkTextPrimary = Color(0xFFEEEEEE); //Putih
+  static const Color darkTextSecondary = Color(0xFFAAAAAA); //Abu-abu terang
+  static const Color darkTextHint = Color(0xFF666666); //Abu-abu
 
   //Line (Divider and Border) Colors  
-  static const Color darkDivider = Color(0xFF3A3A3A);
-  static const Color darkBorder = Color(0xFF3A3A3A);
+  static const Color darkDivider = Color(0xFF3A3A3A); //Abu-abu sedikit gelap
+  static const Color darkBorder = Color(0xFF3A3A3A); //Abu-abu sedikit gelap
   //End of Dark Mode
+
+  /**Fungsi
+   - surface = AppBar, Bottom Nav, Dialog Box, Card, etc **/
 }
