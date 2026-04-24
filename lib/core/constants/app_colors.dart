e9d503941b27d4267ef5bf7ee48888c939b5bfbe
AbdillahFajar <fajarabdillah2264@gmail.com>
2026-04-24 14:00:25 +0700

    core: app colors
    add dark mode

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -7,12 +7,13 @@ class AppColors {
   static const Color primaryDark = Color(0xFF2FA01C); // versi lebih gelap
   static const Color accent = Color(0xFF00C853); // aksen hijau fresh
 
-  //Background Colors
-  static const Color background = Color(0xFFF5F5F5);
-  static const Color surface = Colors.white;
-
   //Error Colors
   static const Color error = Colors.red;
+  //End of Main Colors
+
+  //Light Mode
+  static const Color lightBackground = Color(0xFFF5F5F5);
+  static const Color lightSurface = Colors.white;
 
   //Text Colors
   static const Color textPrimary = Color(0xFF212121);
@@ -22,4 +23,20 @@ class AppColors {
   //Line (Divider and Border) Colors
   static const Color divider = Color(0xFFE0E0E0);
   static const Color border = Color(0xFFE0E0E0);
+  //End of Light Mode
+
+  //Dark Mode
+  static const Color darkBackground = Color(0xFF121212);
+  static const Color darkSurface = Color(0xFF1E1E1E);
+  static const Color darkSurfaceCard = Color(0xFF2C2C2C);
+  
+  //Text Colors
+  static const Color darkTextPrimary = Color(0xFFEEEEEE);
+  static const Color darkTextSecondary = Color(0xFFAAAAAA);
+  static const Color darkTextHint = Color(0xFF666666);
+
+  //Line (Divider and Border) Colors  
+  static const Color darkDivider = Color(0xFF3A3A3A);
+  static const Color darkBorder = Color(0xFF3A3A3A);
+  //End of Dark Mode
 }
